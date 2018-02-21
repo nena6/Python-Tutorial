15c43979e80b1270b61a933d5aabe31919b8e7be
nena6 <nevia.vidakovic@gmail.com>
2018-02-21 20:25:29 +0100

    Exercise 2

---
+++ exercises_2.py
@@ -0,0 +1,27 @@
+#Exercise 2
+
+name = input('Enter your name:\n')
+print('Hello ' + name)
+
+#Exercise 3
+
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+pay = float(hours) * float(rate)
+print('Pay: ' + str(pay))
+
+#Exercise 4
+
+width = 17
+height = 12.0
+
+print(type(width//2))
+print(type(width/2.0))
+print(type(height/3))
+print(type(1+2*5))
+
+#Exercise 5
+
+cels = input('Enter temperature in Celsius: ')
+fahr = float(cels) * 1.8 + 32
+print('Temperature in Fahrenheit is: ' + str(fahr))
