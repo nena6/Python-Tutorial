701dcc5841d21a22d0445b2cfc0e18fbb778c290
nena6 <nevia.vidakovic@gmail.com>
2018-02-22 16:42:47 +0100

    Exercise 4

---
+++ exercises_4.py
@@ -0,0 +1,57 @@
+#Exercise 6
+import sys
+
+def computepay(hours, rate):
+    pay = hours * rate
+    if (float(hours) > 40):
+        extra_hours = float(hours) - 40
+        pay = pay + float(extra_hours) * float(rate) * 1.5
+
+    return pay
+
+try:
+    hours = input('Enter Hours: ')
+    hours = float(hours)
+except:
+    print('Error, please enter numeric input.')
+    sys.exit()
+
+
+try:
+    rate = input('Enter Rate: ')
+    rate = float(rate)
+except:
+    print('Error, please enter numeric input.')
+    sys.exit()
+
+print('Pay: ' + str(computepay(hours, rate)))
+
+#Exercise 7
+
+def computegrade(score):
+
+    if (score >= 0) and (score <= 1):
+        if (score >= 0.9):
+            grade = "A"
+        elif (score >= 0.8):
+            grade = "B"
+        elif (score >= 0.7):
+            grade = "C"
+        elif (score >= 0.6):
+            grade = "D"
+        else:
+            grade = "F"
+    else:
+        grade = 'Bad score'
+
+    return grade
+
+score = input('Enter the score between 0.0 and 1.0: ')
+try:
+    score=float(score)
+except:
+    print('Bad score')
+    sys.exit()
+
+print(computegrade(score))
+
