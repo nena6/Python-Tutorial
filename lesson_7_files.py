95093a96298d010dcae758dbd410d198a0d92b7c
nena6 <nevia.vidakovic@gmail.com>
2018-02-23 21:17:29 +0100

    Exercise 7 - part 2

--- lesson_7_files.py
+++ lesson_7_files.py
@@ -1,3 +1,5 @@
+import time
+
 fhand = open('mbox.txt')
 # count = 0
 #
@@ -5,32 +7,27 @@ fhand = open('mbox.txt')
 #     print('test1')
 #     count += 1
 # print(count)
-print(fhand)
 for line in fhand:
-    print('test2')
-    line = line.rstrip() #kako kad je immutable?
+    #print('test2')
+    line = line.rstrip()
+    print(id(line))
     if line.startswith('From:'):
         print(line)
 
-#fhand = open('mbox.txt') #zasto dvije for petlje ne rade istovremeno???? rade kad se ponovno open napravi, a fhand isti????
-print(fhand)
 #skip uninteresting lines using continue, process interesting lines
 for line in fhand:
-    print('test3')
     line = line.rstrip()
     if not line.startswith('From:'):
         continue
     print(line)
 
-#measure time for both loops
-
-#search through file (same as string using find)
+# #search through file (same as string using find)
 fname = input('Enter filename: ')
 try:
     fhand = open(fname)
 except:
     print('File cannot be opened: %s' % fname)
-    exit() #je li potrebno? razlika od sys.exit()
+    exit()
 for line in fhand:
     line=line.rstrip()
     if line.find('@uct.ac.za') == -1:
